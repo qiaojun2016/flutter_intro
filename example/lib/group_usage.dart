27fe522b96ab9052dc651ca931b535275a728ef7
Jinyu <cadenho@hotmail.com>
2024-10-24 13:36:01 +0800

    add linux and web platform support

--- example/lib/group_usage.dart
+++ example/lib/group_usage.dart
@@ -8,67 +8,70 @@ class GroupUsage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: List.generate(
-                3,
-                (index) => IntroStepBuilder(
-                  order: index,
-                  text: 'group: default, order: $index',
-                  builder: (context, key) {
-                    return Text(
-                      'group: default, order: $index',
-                      key: key,
-                    );
-                  },
-                ),
-              ).toList(),
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: List.generate(
-                3,
-                (index) => IntroStepBuilder(
-                  group: 'other',
-                  order: index,
-                  text: 'group: other, order: $index',
-                  builder: (context, key) {
-                    return Text(
-                      'group: other, order: $index',
-                      key: key,
-                    );
-                  },
-                ),
-              ).toList(),
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Intro.of(context).start();
-                  },
-                  child: const Text('Group: default'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Intro.of(context).start(
-                      group: 'other',
-                    );
-                  },
-                  child: const Text('Group: other'),
-                ),
-              ],
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.only(right: 100),
+        child: Center(
+          child: Column(
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: List.generate(
+                  3,
+                  (index) => IntroStepBuilder(
+                    order: index,
+                    text: 'group: default, order: $index',
+                    builder: (context, key) {
+                      return Text(
+                        'group: default, order: $index',
+                        key: key,
+                      );
+                    },
+                  ),
+                ).toList(),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: List.generate(
+                  3,
+                  (index) => IntroStepBuilder(
+                    group: 'other',
+                    order: index,
+                    text: 'group: other, order: $index',
+                    builder: (context, key) {
+                      return Text(
+                        'group: other, order: $index',
+                        key: key,
+                      );
+                    },
+                  ),
+                ).toList(),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Intro.of(context).start();
+                    },
+                    child: const Text('Group: default'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Intro.of(context).start(
+                        group: 'other',
+                      );
+                    },
+                    child: const Text('Group: other'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
